3bd705eedb24d9cc441d0b0ec61b13fbbed53c5d
Lobos-Alamedas <lobos.alamedas2024@gmail.com>
2024-11-11 13:14:59 -0600

    Add files via upload

---
+++ for.js
@@ -0,0 +1,7 @@
+console.log("Ciclo for");       // Titulo.
+
+for(var i=100;i>=1;i=i-1){      // Incio de for.
+
+    console.log(i);             // Imprime en consola.
+
+}                               // Fin de for.
---
+++ while.js
@@ -0,0 +1,12 @@
+console.log("Ciclo while");       // Titulo.
+
+var i2 = 100;                 
+
+while(i2>=1){
+
+    console.log(i2);
+
+    i2 = i2 - 1;
+
+}
+
